be2213ac8ba6a80ae2e4da5be95fa7bf5f12c294
tchfrnkln <tchfrnkln@gmail.com>
2025-12-23 23:47:24 +0100

    Feat:Login Start

---
+++ app/auth/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+  return (
+    <div className="p-6">page</div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -52,11 +52,10 @@ export default function Home() {
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            href="/auth"
             rel="noopener noreferrer"
           >
-            Documentation
+            Get Started
           </a>
         </div>
       </main>
