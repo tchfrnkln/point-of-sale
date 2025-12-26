41e9dc6e176b25fa4824674b065836a5659863c0
tchfrnkln <tchfrnkln@gmail.com>
2025-12-26 01:12:56 +0100

    Electron build Test: Stable

---
+++ electron/dist/main.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+const path_1 = __importDefault(require("path"));
+let mainWindow = null;
+function createWindow() {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            preload: path_1.default.join(__dirname, "preload.js")
+        }
+    });
+    // Dev URL
+    mainWindow.loadURL("http://localhost:3000");
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+electron_1.app.whenReady().then(createWindow);
+electron_1.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin")
+        electron_1.app.quit();
+});
---
+++ electron/main.ts
@@ -0,0 +1,27 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
+  });
+
+  // Dev URL
+  mainWindow.loadURL("http://localhost:3000");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
